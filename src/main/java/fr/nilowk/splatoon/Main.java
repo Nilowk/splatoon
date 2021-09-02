90b4c166ddc4fb9622930847547e1c4cddd01a47
Nilowk <rayotantoine@gmail.com>
2021-09-02 09:40:44 +0200

    ajout du sniper

--- src/main/java/fr/nilowk/splatoon/Main.java
+++ src/main/java/fr/nilowk/splatoon/Main.java
@@ -2,6 +2,7 @@ package fr.nilowk.splatoon;
 
 import fr.nilowk.splatoon.test.Gun;
 import fr.nilowk.splatoon.test.Rouleau;
+import fr.nilowk.splatoon.test.Sniper;
 import fr.nilowk.splatoon.test.Test;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,6 +16,7 @@ public class Main extends JavaPlugin {
         pm.registerEvents(new Test(this), this);
         pm.registerEvents(new Gun(this), this);
         pm.registerEvents(new Rouleau(this), this);
+        pm.registerEvents(new Sniper(this), this);
 
     }
 
---
+++ src/main/java/fr/nilowk/splatoon/test/Sniper.java
@@ -0,0 +1,72 @@
+package fr.nilowk.splatoon.test;
+
+import fr.nilowk.splatoon.Main;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Sniper implements Listener {
+
+    private Main instance;
+
+    public Sniper(Main instance) {
+
+        this.instance = instance;
+
+    }
+
+    @EventHandler
+    public void chargeBow(EntityShootBowEvent event) {
+
+        if (event.getEntityType() == EntityType.PLAYER) {
+
+            Player player = (Player) event.getEntity();
+
+            if (event.getForce() < 1.0f) {
+
+                event.setCancelled(true);
+                return;
+
+            }
+
+            if (player.getExp() < 0.72f) {
+
+                event.setCancelled(true);
+                return;
+
+            }
+
+            player.setExp(player.getExp() - 0.72f);
+
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+
+                    Block block = event.getProjectile().getLocation().getBlock().getRelative(BlockFace.DOWN);
+                    while (block.getType() == Material.AIR) {
+
+                        block = block.getRelative(BlockFace.DOWN);
+
+                    }
+                    if (block.getType() == Material.CYAN_TERRACOTTA) {
+
+                        block.setType(Material.ORANGE_WOOL);
+
+                    }
+                }
+
+            }.runTaskTimer(instance, 0 , 1);
+
+        }
+
+    }
+
+}
